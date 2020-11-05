7840bc046c0969d1d8b4533e3f1613fdf73077d3
Beatriz Longley <beatrizsousacosta@gmail.com>
2020-11-05 10:13:08 +0100

    recaptcha set up

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,7 +6,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
 
-    if @contact.save
+    if @contact.save && verify_recaptcha(model: @contact)
       message = ContactMailer.with(contact: @contact).new_contact
       message.deliver_now
       redirect_to contact_path, notice: "We have received your contact form and will be in touch soon!"
@@ -15,6 +15,16 @@ class ContactsController < ApplicationController
       render :new, notice: "There was an error sending your contact form. Please try again."
     end
   end
+
+  # respond_to do |format|
+  #   if verify_recaptcha(model: @message) && @message.save
+  #     format.html { redirect_to @message, notice: 'Message was successfully created.' }
+  #     format.json { render :show, status: :created, location: @message }
+  #   else
+  #     format.html { render :new }
+  #     format.json { render json: @message.errors, status: :unprocessable_entity }
+  #   end
+  # end
 
   private
 
